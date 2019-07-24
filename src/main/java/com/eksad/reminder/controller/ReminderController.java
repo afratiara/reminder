225a42ceadd9ee286087519576739c0514018c50
afratiara <afratiararahmayani@yahoo.co.id>
2019-07-25 00:09:59 +0700

    CRUD with swagger

--- src/main/java/com/eksad/reminder/controller/ReminderController.java
+++ src/main/java/com/eksad/reminder/controller/ReminderController.java
@@ -5,7 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,14 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.eksad.reminder.dao.ReminderDao;
 import com.eksad.reminder.entity.Reminder;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
-@RequestMapping("reminder")
+@RequestMapping(value = "/api/v1")
+@Api(tags = "Reminder")
 public class ReminderController {
 	
 	@Autowired
 	ReminderDao reminderDao;
 	
-	@RequestMapping("getAll")
+	@ApiOperation(
+			value = "API to retrieve reminder's data",
+			notes = "Return data with JSON format",
+			tags = "Get Data API")
+	@GetMapping("/reminder")
 	public List<Reminder> getAll() {
 		List<Reminder> result = new ArrayList<>();
 		reminderDao.findAll().forEach(result::add);
@@ -31,14 +43,22 @@ public class ReminderController {
 
 // ------------------------------------------------
 	
-	@RequestMapping("getOne/{id}")
+	@ApiOperation(
+			value = "API to retrieve all reminder by id",
+			notes = "Return data with JSON format",
+			tags = "Get Data API")
+	@GetMapping("/reminder/{id}")
 	public Reminder getOne(@PathVariable Long id) {
 		return reminderDao.findById(id).orElse(null);
 	}
 	
 // ------------------------------------------------
 	
-	@RequestMapping(value = "save", method = RequestMethod.POST)
+	@ApiOperation(
+			value = "Insert data reminder",
+			notes = "Insert data reminder to database",
+			tags = "Data Manipulation API")
+	@PostMapping(value = "/reminder")
 	public Reminder save(@RequestBody Reminder reminder) {
 		try {
 			return reminderDao.save(reminder);
@@ -51,7 +71,11 @@ public class ReminderController {
 	
 // ------------------------------------------------
 	
-	@RequestMapping(value = "update/{id}", method = RequestMethod.PUT)
+	@ApiOperation(
+			value = "Update data reminder",
+			notes = "Update data reminder to database",
+			tags = "Data Manipulation API")
+	@PutMapping(value = "reminder/{id}")
 	public Reminder update(@RequestBody Reminder reminder, @PathVariable Long id) {
 		
 		Reminder reminderSelected = reminderDao.findById(id).orElse(null);
@@ -68,7 +92,11 @@ public class ReminderController {
 	
 // ------------------------------------------------
 	
-	@RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE)
+	@ApiOperation(
+			value = "Delete data reminder",
+			notes = "Delete data reminder from database",
+			tags = "Data Manipulation API")
+	@DeleteMapping(value = "reminder/{id}")
 	public HashMap<String, Object> delete(@PathVariable Long id) {
 		HashMap<String, Object> result = new HashMap<String, Object>();
 		reminderDao.deleteById(id);
